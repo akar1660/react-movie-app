bf362b3bb3d9c4a3a874f78db150ad3d5f8736b1
Joseph <alisanakar1@gmail.com>
2022-10-30 17:28:10 -0400

    v15

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import AuthContextProvider from './context/AuthContext'
 import AppRouter from './router/AppRouter'
 
 const App = () => {
   return (
     <div>
-        <AppRouter/>
+        <AuthContextProvider>
+            <AppRouter/>
+        </AuthContextProvider>
+        
     </div>
   )
 }
