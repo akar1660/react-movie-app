4fc4261b55bdad2741d6328c62ee2da7919b2d4a
Joseph <alisanakar1@gmail.com>
2022-10-30 17:11:27 -0400

    v13

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,15 @@
 import React, {useState} from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import { signIn } from '../auth/firebase'
 const Login = () => {
 
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
-
+  const navigate = useNavigate();
   const handleLogin = (e) =>{
     e.preventDefault();
-    console.log(email, password);
+    signIn(email,password, navigate)
+    // console.log(email, password);
   }
   
   return (
