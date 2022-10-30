8d6391b38260e5fb021fe1b67ddb28614364adeb
Joseph <alisanakar1@gmail.com>
2022-10-30 13:31:37 -0400

    v7

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Register = () => {
+
+  const [firstName, setFirstName] = useState()
+  const [lastName, setLastName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
   return (
     <div className='d-flex justify-content-center'>
       <div className="form-image d-none d-md-block">
@@ -11,19 +17,19 @@ const Register = () => {
         <form id='register'>
           <div className="mb-3">
             <label htmlFor="last-name" className='form-label'>First Name</label>
-            <input type="text" className='form-control' id='firstName' placeholder='Enter your first name..' required/>
+            <input type="text" className='form-control' id='firstName' placeholder='Enter your first name..' required onChange={(e) => setFirstName(e.target.value)}/>
           </div>
           <div className="mb-3">
             <label htmlFor="last-name" className='form-label'>Last Name</label>
-            <input type="text" className='form-control' id='lastName' placeholder='Enter your last name..' required/>
+            <input type="text" className='form-control' id='lastName' placeholder='Enter your last name..' required onChange={(e) => setLastName(e.target.value)}/>
           </div>
           <div className="mb-3">
             <label htmlFor="email" className='form-label'>Email</label>
-            <input type="email" className='form-control' id='email' placeholder='Enter your email address..' required/>
+            <input type="email" className='form-control' id='email' placeholder='Enter your email address..' required onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <div className="mb-3">
             <label htmlFor="password" className='form-label'>Password</label>
-            <input type="password" className='form-control' id='firstName' placeholder='Enter your password..' required/>
+            <input type="password" className='form-control' id='firstName' placeholder='Enter your password..' required onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <div className="mb-3">
             <input type="submit" className='btn btn-primary form-control' value="Register"/>
