f85726944f18ee5bde15dcbd8bfd247691d7a035
Joseph <alisanakar1@gmail.com>
2022-10-30 17:07:13 -0400

    v12

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,31 +1,129 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword  } from "firebase/auth";
-
+import { initializeApp } from 'firebase/app';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from 'firebase/auth';
+import {
+  toastErrorNotify,
+  toastSuccessNotify,
+  toastWarnNotify,
+} from '../helpers/ToastNotify';
 
 // TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+//! firebase console settings bölümünden firebaseconfig ayarlarını al
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId ,
+  projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_apiId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async(email, password) =>{
-    //? It is a way to create a new Firebase method
-    try {
-        let userCredential=await createUserWithEmailAndPassword(auth, email, password)
-        console.log(userCredential);
-    } catch (err) {
-        console.log(err)
+export const createUser = async (email, password, navigate, displayName) => {
+  //? To create a new Firebase method
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    //? To update firebase method
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    toastSuccessNotify('Registered successfully!');
+    navigate('/');
+    console.log(userCredential);
+  } catch (err) {
+    toastErrorNotify(err.message);
+  }
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
+//! With Email/password enable login 
+export const signIn = async (email, password, navigate) => {
+  //? It is a firebase method to login for current users.
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate('/');
+    toastSuccessNotify('Logged in successfully!');
+    // sessionStorage.setItem('user', JSON.stringify(userCredential.user));
+    console.log(userCredential);
+  } catch (err) {
+    toastErrorNotify(err.message);
+    console.log(err);
+  }
+};
+
+export const userObserver = (setCurrentUser) => {
+  //? This method allows us to check whether user signed in or not. If a user is changed, this method creates a new response for the new user. 
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      // User is signed out
+      setCurrentUser(false);
     }
+  });
+};
+
+export const logOut = () => {
+  signOut(auth);
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Google
+//! Google ile girişi enable yap
+//* => Authentication => sign-in-method => Authorized domains => add domain
+//! Projeyi deploy ettikten sonra google sign-in çalışması için domain listesine deploy linkini ekle
+export const signUpProvider = (navigate) => {
+  //? This method allows us to sign in with Google 
+  const provider = new GoogleAuthProvider();
+  //? With this method we can sign in with new opened window 
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate('/');
+      toastSuccessNotify('Logged out successfully!');
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+    });
+};
+
+export const forgotPassword = (email) => {
+  //? This method allows us to recreate a new password if users forget the password.
+  sendPasswordResetEmail(auth, email)
+    .then(() => {
+      // Password reset email sent!
+      toastWarnNotify('Please check your mail box!');
+      // alert("Please check your mail box!");
+    })
+    .catch((err) => {
+      toastErrorNotify(err.message);
+      // alert(err.message);
+      // ..
+    });
 };
