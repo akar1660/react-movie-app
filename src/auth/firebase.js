3a34b4fafa64ce99fddf67a294bae27d2528f870
Joseph <alisanakar1@gmail.com>
2022-10-30 16:17:06 -0400

    v11

---
+++ src/auth/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword  } from "firebase/auth";
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId ,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_apiId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export const createUser = async(email, password) =>{
+    //? It is a way to create a new Firebase method
+    try {
+        let userCredential=await createUserWithEmailAndPassword(auth, email, password)
+        console.log(userCredential);
+    } catch (err) {
+        console.log(err)
+    }
+};
