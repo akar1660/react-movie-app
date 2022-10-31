2912a533256d90bc766fa23706d850720687e32b
Joseph <alisanakar1@gmail.com>
2022-10-31 02:00:06 -0400

    v18

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios';
+import MovieCard from '../components/MovieCard';
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
@@ -13,14 +14,14 @@ const Main = () => {
 
   const getMovies = (API) => { 
     setLoading(true)
-    axios.get(API).then(res => console.log(res.data.results)).catch(err => console.log(err)).finally(()=> setLoading(false));
+    axios.get(API).then(res => setMovies(res.data.results)).catch(err => console.log(err)).finally(()=> setLoading(false));
    }
   return (
     <div className='d-flex justify-content-center flex-wrap'>
       {loading ? <div className='spinner-border text-primary m-5' role="status">
         <span className='sr-only'>Loading...</span>
       </div> :(
-        movies?.map(movie => null)
+        movies?.map(movie => <MovieCard key={movie.id} {...movie}/>)
         )}
     </div>
   )
